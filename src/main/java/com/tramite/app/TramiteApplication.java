6563bd06d0fffbfc2c127b5b15b418f8295c5203
Vladimir <vladimir_sc@hotmail.com>
2021-06-15 16:22:27 -0500

    ultimo cambios 99.5

--- src/main/java/com/tramite/app/TramiteApplication.java
+++ src/main/java/com/tramite/app/TramiteApplication.java
@@ -1,48 +1,26 @@
 package com.tramite.app;
 
-import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import com.tramite.app.Entidades.Perfiles;
 
 @SpringBootApplication 
-public class TramiteApplication implements CommandLineRunner {
-	
-	Logger logger = LoggerFactory.getLogger(getClass());
-	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	
-	@Autowired
-	private BCryptPasswordEncoder encriptar;
-
-	public static void main(String[] args) {
-		SpringApplication.run(TramiteApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		logger.info("================ CARGA INICIAL ============");
-		String sql ="SELECT * FROM PERFIL";
-		List<Perfiles> perfiles= jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Perfiles.class));
-		perfiles.forEach(System.out :: println);
-		
-		String clave = "12345";
-		
-		for(int i=0;i<5;i++) {
-			String clave_encriptada = encriptar.encode(clave);
-			logger.info(i+"= "+clave_encriptada);
+public class TramiteApplication extends SpringBootServletInitializer { 
+	 
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+			// TODO Auto-generated method stub
+			return builder.sources(TramiteApplication.class);
 		}
-	}
+	 
 
-}
+		public static void main(String[] args) {
+			SpringApplication.run(TramiteApplication.class, args);
+		}
+		
+	 
+
+	}
